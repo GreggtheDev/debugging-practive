19807c2569d8c6988415d3b17c51bc5654a95f51
GreggtheDev <greggthedev@gmail.com>
2024-08-05 12:30:26 -0400

    created AverageCalculator class to average array of integers

---
+++ src/main/java/org/example/AverageCalculator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class AverageCalculator {
+
+    public static double calculateAverage(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return (double) sum / numbers.length;
+    }
+
+    public static void main(String[] args) {
+        int[] testArray = {1, 2, 3, 4, 5};
+        System.out.println("The average is: " + calculateAverage(testArray));
+    }
+}
+
